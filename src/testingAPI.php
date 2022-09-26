38e729b70ec8f90a7028fe13614ff2a00af44298
Joe <kuttehj@etown.edu>
2022-09-25 21:47:14 -0400

    adding comments to testingAPI.php

--- src/testingAPI.php
+++ src/testingAPI.php
@@ -1,5 +1,6 @@
 <?PHP
 
+//This file is just for testing a free api that will let us pull data from yahoo finance
 
 $url = "https://query1.finance.yahoo.com/v8/finance/chart/AAPL?region=US&lang=en-US&includePrePost=false&interval=1h&useYfid=true&range=1d";
 $stock_data = json_decode(file_get_contents($url), true);
