18956a51e3912bda938e4a4e87fef1173aee3502
Joe <kuttehj@etown.edu>
2022-09-29 08:58:27 -0400

    added help page

---
+++ help.php
@@ -0,0 +1,9 @@
+<?PHP
+require("functions/basic_html_functions.php");
+require("includes/header.php");
+display_small_page_heading("Help","");
+
+echo " help page stuff here";
+
+require("includes/footer.php");
+?>
