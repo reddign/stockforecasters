e7b0172225cd6f34199ff569c0aba079111be165
Joe <kuttehj@etown.edu>
2022-09-29 09:01:42 -0400

    html stuff

--- src/about.php
+++ src/about.php
@@ -2,13 +2,19 @@
 require("functions/basic_html_functions.php");
 require("includes/header.php");
 display_small_page_heading("About","");
+?>
 
-echo " This is a semester long project for Software Engineering (EGR341) at Elizabethtown College. Over the next 12 weeks
+<html>
+
+<h5> This is a semester long project for Software Engineering (EGR341) at Elizabethtown College. Over the next 12 weeks
 Blake Hildebrand, Joe Kutteh, and Nathan Griffin take on the stock market in this exciting forcasting project!
 Blake will be doing the Beginner Page.
 Nathan will be doing the Intermediate Page.
 Joe will be doing the Advanced Page.
-More will be added here as it becomes available.";
+More will be added here as it becomes available. </h5>
 
+</html>
+
+<?PHP
 require("includes/footer.php");
 ?>
--- src/help.php
+++ src/help.php
@@ -2,8 +2,12 @@
 require("functions/basic_html_functions.php");
 require("includes/header.php");
 display_small_page_heading("Help","");
+?>
 
-echo " help page stuff here";
+<html>
+    <h5>help page stuff here</h5>
+</html>
 
+<PHP
 require("includes/footer.php");
 ?>
