4970cca28004c15823bbcd2868e755f51b0cfabd
Joe <kuttehj@etown.edu>
2022-09-29 16:38:28 -0400

    time

--- src/testingAPI.php
+++ src/testingAPI.php
@@ -8,21 +8,24 @@ $stock_data['chart']['result'][0]['meta']['regularMarketPrice'];
 echo $stock_data['chart']['result'][0]['meta']['regularMarketPrice'];
 
 ?>
+<script>function display() {
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 
-<script>function display_ct6() {
 var x = new Date()
 var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
 hours = x.getHours( ) % 12;
 hours = hours ? hours : 12;
-var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getFullYear(); 
+var x1=x.monthNames[date.getMonth()] + 1+ "/" + x.getDate() + "/" + x.getFullYear(); 
 x1 = x1 + " - " +  hours + ":" +  x.getMinutes() + ":" +  x.getSeconds() + ":" + ampm;
 document.getElementById('ct6').innerHTML = x1;
-display_c6();
+display();
  }
-function display_c6(){
+function display(){
 var refresh=1000; // Refresh rate in milli seconds
-mytime=setTimeout('display_ct6()',refresh)
+mytime=setTimeout('display()',refresh)
 }
-display_c6()
+display()
 </script>
-<h id='ct6'></h>
+<span id='ct6'></span>
