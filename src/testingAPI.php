146b288f62b99df96a8d789e6faf37ebf84d29c7
Joe <kuttehj@etown.edu>
2022-09-29 16:28:37 -0400

    time

--- src/testingAPI.php
+++ src/testingAPI.php
@@ -8,3 +8,27 @@ $stock_data['chart']['result'][0]['meta']['regularMarketPrice'];
 echo $stock_data['chart']['result'][0]['meta']['regularMarketPrice'];
 
 ?>
+
+
+<html>
+<head>
+<title>(Type a title for your page here)</title>
+<script type="text/javascript"> 
+function display_c(){
+var refresh=1000; // Refresh rate in milli seconds
+mytime=setTimeout('display_ct()',refresh)
+}
+
+function display_ct() {
+var x = new Date()
+document.getElementById('ct').innerHTML = x;
+display_c();
+ }
+</script>
+</head>
+
+<body onload=display_ct();>
+<span id='ct' ></span>
+
+</body>
+</html>
