5945bdcd25d6671e01379d52b3c667b066a3404d
Nathan Griffin <griffinn@etown.edu>
2022-09-29 08:49:49 -0400

    intermediate page update

--- src/intermediate.php
+++ src/intermediate.php
@@ -2,6 +2,9 @@
 require("functions/basic_html_functions.php");
 require("includes/header.php");
 display_small_page_heading("Intermediate","");
+
+
+<input type="text" id="Intstock" name="Intstock"> <br> <br>
  
 require("includes/footer.php");
 ?>
