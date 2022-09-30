7130c2fa3ce8301d32bea100b1d945216421c616
Nathan Griffin <griffinn@etown.edu>
2022-09-30 11:48:57 -0400

    intermediate Page updates

--- src/intermediate.php
+++ src/intermediate.php
@@ -8,7 +8,7 @@ display_small_page_heading("Intermediate","");
 <html>
     <form>
     <label for="Intstock">Stock Search: </label>
-        <input type="text" id="Intstock" name="Intstock"> <br> <br>
+        <input style="width:250px; border-width:3px border-style:solid; border-color:black;" type="text" id="Intstock" name="Intstock"> <br> <br>
     </form>
 </html>
 
