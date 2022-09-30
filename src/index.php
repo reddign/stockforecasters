ad3722df5f02009ebe9b5e3e660001b70c5eb08f
Joe <kuttehj@etown.edu>
2022-09-29 20:06:10 -0400

    Time/news

--- src/index.php
+++ src/index.php
@@ -9,31 +9,46 @@ display_small_page_heading("EC Stock Forecasters");
     <h4 style="text-align:center">
       <b>
 
-      <script>function display_t() {
-      var x = new Date()
-      var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
-      hours = x.getHours( ) % 12;
-      hours = hours ? hours : 12;
-      var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getFullYear(); 
-      x1 = x1 + "  " +  hours + ":" +  x.getMinutes() + ":" +  x.getSeconds() + ampm;
-      document.getElementById('ct6').innerHTML = x1;
-      display();
-      }
-      function display(){
-      var refresh=1000; // Refresh rate in milli seconds
-      mytime=setTimeout('display_ct6()',refresh)
-      }
-      display()
+      <script>function display_ct7() {
+        var x = new Date()
+        var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
+        hours = x.getHours( ) % 12;
+        hours = hours ? hours : 12;
+        hours=hours.toString().length==1? 0+hours.toString() : hours;
+
+        var minutes=x.getMinutes().toString()
+        minutes=minutes.length==1 ? 0+minutes : minutes;
+
+        var seconds=x.getSeconds().toString()
+        seconds=seconds.length==1 ? 0+seconds : seconds;
+
+        var month=(x.getMonth() +1).toString();
+        month=month.length==1 ? 0+month : month;
+
+        var dt=x.getDate().toString();
+        dt=dt.length==1 ? 0+dt : dt;
+
+        var x1=month + "/" + dt + "/" + x.getFullYear(); 
+        x1 = x1 + " - " +  hours + ":" +  minutes + ":" +  seconds + " " + ampm;
+        document.getElementById('ct7').innerHTML = x1;
+        display_c7();
+        }
+        function display_c7(){
+        var refresh=1000; // Refresh rate in milli seconds
+        mytime=setTimeout('display_ct7()',refresh)
+        }
+        display_c7()
       </script>
-      <span id='ct6'></span>
+      <span id='ct7' style="background-color: #FFFF00"></span>
 
       </b>
       <br><br> Welcome to the main page of the EC Stock Forecasters <br> <br>
     </h4>
     <marquee style="font-size:35pt;" behavior="scroll" direction="left" scrollamount="7">Dow <img width="20px" src="<?php echo $path; ?>images/redArrow.png">  Nasdaq <img width="20px" src="<?php echo $path; ?>images/greenArrow.png">   S&P500   Russell 2000   </marquee>
 
-
     <h2> News </h2>
+
+
   </html>
   
 
