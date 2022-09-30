0cd447de7e3273c41cdfb6389eae8ac00c244ba1
Nathan Griffin <griffinn@etown.edu>
2022-09-30 12:01:42 -0400

    intermediate Page updates 1.1

--- src/intermediate.php
+++ src/intermediate.php
@@ -9,7 +9,7 @@ display_small_page_heading("Intermediate","");
     <form>
     <label for="Intstock">Enter Stock Symbol: </label>
         <input style="width:250px; border-width:3px border-style:solid; border-color:black;" type="text" id="Intstock" name="Intstock"> 
-        <label for="time-frame">Choose a dog name:</label>
+        <label for="time-frame">Choose a time frame:</label>
         <select name="time-frame" id="time-frame">
         <option value="1 Day">day</option>
         <option value="1 Week">week</option>
