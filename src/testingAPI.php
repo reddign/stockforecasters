7682d0f7153a808cfbbc61dc8ab6c0a22a9cd31f
Joe <kuttehj@etown.edu>
2022-10-01 15:40:23 -0400

    added new paths to access data in api

--- src/testingAPI.php
+++ src/testingAPI.php
@@ -1,10 +1,13 @@
 <?PHP
 
-//This file is just for testing a free api that will let us pull data from yahoo finance
 
 $url = "https://query1.finance.yahoo.com/v8/finance/chart/AAPL?region=US&lang=en-US&includePrePost=false&interval=1h&useYfid=true&range=1d";
 $stock_data = json_decode(file_get_contents($url), true);
-$stock_data['chart']['result'][0]['meta']['regularMarketPrice'];
+
 echo $stock_data['chart']['result'][0]['meta']['regularMarketPrice'];
+echo "<br>";
+echo $stock_data['chart']['result'][0]['timestamp'][0];
+echo "<br>";
+echo $stock_data['chart']['result'][0]['indicators']['quote'][0]['open'][7];
 
 ?>
