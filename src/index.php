fdfa078f25a21b7517b7244d9f8f189836e3694f
Joe <kuttehj@etown.edu>
2022-10-05 00:14:59 -0400

    Updating index prices on main page

--- src/index.php
+++ src/index.php
@@ -3,7 +3,47 @@ $path = '';
 require("functions/basic_html_functions.php");
 require("includes/header.php");
 display_small_page_heading("EC Stock Forecasters");
-  ?>
+
+#color code green=0 red=1 black=2
+$DJI_url = 'https://query1.finance.yahoo.com/v8/finance/chart/^DJI?region=US&lang=en-US&includePrePost=false&interval=1h&useYfid=true&range=1d';
+$DJI_stock_data = json_decode(file_get_contents($DJI_url), true);
+$DJI_current = $DJI_stock_data['chart']['result'][0]['meta']['regularMarketPrice'];
+$DJI_prevClose = $DJI_stock_data['chart']['result'][0]['meta']['previousClose'];
+$DJI_diff = number_format($DJI_current - $DJI_prevClose, 2);
+$DJI_diffPercent = number_format((($DJI_current - $DJI_prevClose) / $DJI_prevClose) * 100, 2);
+if($DJI_diff > 0) {
+
+
+  
+  $DJI_output = $DJI_current." +".$DJI_diff." (".$DJI_diffPercent."%)";
+  //$DJI_color = 0;
+} else if($DJI_diff < 0) {
+  #<img width="20px" src="images/redArrow.png">
+  $DJI_output = $DJI_current." ".$DJI_diff." (".$DJI_diffPercent."%)";
+  //$DJI_color = 1;
+} else {
+  $DJI_output = $DJI_current." ".$DJI_diff." (".$DJI_diffPercent."%)";
+  //$DJI_color = 2;
+}
+
+$NASDAQ_url = 'https://query1.finance.yahoo.com/v8/finance/chart/^IXIC?region=US&lang=en-US&includePrePost=false&interval=1h&useYfid=true&range=1d';
+$NASDAQ_stock_data = json_decode(file_get_contents($NASDAQ_url), true);
+$NASDAQ_current = $NASDAQ_stock_data['chart']['result'][0]['meta']['regularMarketPrice'];
+$NASDAQ_prevClose = $NASDAQ_stock_data['chart']['result'][0]['meta']['previousClose'];
+$NASDAQ_diff = number_format($NASDAQ_current - $NASDAQ_prevClose, 2);
+$NASDAQ_diffPercent = number_format((($NASDAQ_current - $NASDAQ_prevClose) / $NASDAQ_prevClose) * 100, 2);
+if($NASDAQ_diff > 0) {
+  $NASDAQ_output = $NASDAQ_current." +".$NASDAQ_diff." (".$NASDAQ_diffPercent."%)";
+  //$NASDAQ_color = 0;
+} else if($NASDAQ_diff < 0) {
+  #<img width="20px" src="images/redArrow.png">
+  $NASDAQ_output = $NASDAQ_current." ".$NASDAQ_diff." (".$NASDAQ_diffPercent."%)";
+  //$NASDAQ_color = 1;
+} else {
+  $NASDAQ_output = $NASDAQ_current." ".$NASDAQ_diff." (".$NASDAQ_diffPercent."%)";
+  //$NASDAQ_color = 2;
+}
+?>
 
   <html>
     <h4 style="text-align:center">
@@ -43,15 +83,21 @@ display_small_page_heading("EC Stock Forecasters");
 
       </b>
       <br><br> Welcome to the main page of the EC Stock Forecasters <br> <br>
-    </h4>
-    
-    <marquee style="font-size:35pt;" behavior="scroll" direction="left" scrollamount="7">Dow <img width="20px" src="<?php echo $path; ?>images/redArrow.png">  Nasdaq <img width="20px" src="<?php echo $path; ?>images/greenArrow.png">   S&P500   Russell 2000   </marquee>
+      </h4>
+
+
+      
+ 
+    <marquee style="font-size:35pt;" behavior="scroll" direction="left" scrollamount="7"> Dow <?php echo $DJI_current."<span style='color:green;'> +$DJI_diff ($DJI_diffPercent%)</span>"; ?> Nasdaq <?php echo $NASDAQ_output;?> S&P500 Russell 2000 </marquee>
 
     <h2> News </h2>
-
 
   </html>
   
 
 <?PHP
 require("includes/footer.php");
+
+?>
+
+
