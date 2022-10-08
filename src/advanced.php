bd70059f5f505e926fa85d0c1b41dca5941c91de
Joe <kuttehj@etown.edu>
2022-10-08 11:17:22 -0400

    adding graphs

--- src/advanced.php
+++ src/advanced.php
@@ -65,56 +65,6 @@ if (isset($_GET['Search'])) {
     displayGraph($stockName, $dates, $prices);
 ?>
 
-    <!-- <html>
-
-    <div>
-        <canvas id="stockChart" height="100px"></canvas>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script>
-        const labels = <?php echo json_encode($dates);?>;
-
-        const data = {
-            labels: labels,
-            datasets: [{
-                label: "<?php echo $stockName;?>",
-                backgroundColor: 'rgb(0, 188, 212)',
-                borderColor: 'rgb(0, 188, 212)',
-                data: <?php echo json_encode($prices);?>,
-            }]
-        };
-
-        const config = {
-            type: 'line',
-            data: data,
-            options: {
-                plugins: {
-                    legend: {
-                        display: true
-                    }
-                }, elements: {
-                    point: {
-                        radius: 0
-                    }
-                },
-
-                hover: {
-                    mode: 'index',
-                    intersect: false
-                }
-
-            }
-        };
-        const myChart = new Chart(
-            document.getElementById('stockChart'),
-            config
-        );
-    </script>
-
-
-
-    </html> -->
-
 <?PHP
 }
 
