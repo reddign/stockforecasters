5ecc01f30b86a688abcbd312a7e99a60e636e110
Joe <kuttehj@etown.edu>
2022-10-09 23:42:13 -0400

    adding graph features

--- src/advanced.php
+++ src/advanced.php
@@ -11,7 +11,7 @@ date_default_timezone_set('America/New_York');
 <body>
     <form method="get">
         <label for="Intstock">Stock Symbol: </label>
-        <input style="width:150px; border-width:3px border-style=solid; border-color:black;" type="text" id="Intstock" name="Intstock"> <br><br>
+        <input style="width:150px; border-width:3px border-style=solid; border-color:black;" type="text" id="Intstock" name="Intstock">
         <label for="timeframe">Time frame:</label>
         <select name="timeframe" id="timeframe">
             <option value="none" selected disabled hidden>n/a</option>
@@ -26,11 +26,10 @@ date_default_timezone_set('America/New_York');
             <option value="10y">10 Years</option>
             <option value="ytd">Year to Date</option>
             <option value="max">Max</option>
-        </select> <br><br>
+        </select>
         <input type="submit" name="Search" id="Search" value="Search" /> <br><br>
     </form>
 </body>
-
 <?PHP
 
 if (isset($_GET['Search'])) {
@@ -46,7 +45,11 @@ if (isset($_GET['Search'])) {
         $prices[$i] =  $url[3]['chart']['result'][0]['indicators']['quote'][0]['close'][$i]; #0=930 1=1030 2=1130 3=1230 4=130 5=230 6=330 7=400
     }
 
-    displayGraph($stockName, $dates, $prices);
+    $prevClose = $url[3]['chart']['result'][0]['meta']['chartPreviousClose'];
+
+    echo $prevClose;
+
+    displayGraph($stockName, $dates, $prices,$prevClose);
 
 ?>
 
--- src/functions/graphingFunctions.php
+++ src/functions/graphingFunctions.php
@@ -1,6 +1,6 @@
 <?PHP
 
-function displayGraph($stockName = "", $dates, $prices)
+function displayGraph($stockName = "", $dates, $prices,$prevClose)
 {
 
 ?>
@@ -64,7 +64,7 @@ function displayGraph($stockName = "", $dates, $prices)
     </html>
 
 <?PHP
-
+displayName($stockName, $dates, $prices,$prevClose);
 }
 
 function timeInterval($stockName, $time)
@@ -73,7 +73,7 @@ function timeInterval($stockName, $time)
 
     switch ($time) {
         case "1d": {
-                $url[0] = 'https://query1.finance.yahoo.com/v8/finance/chart/' . $stockName . '?region=US&lang=en-US&includePrePost=false&interval=30m&useYfid=true&range=' . $time;
+                $url[0] = 'https://query1.finance.yahoo.com/v8/finance/chart/' . $stockName . '?region=US&lang=en-US&includePrePost=false&interval=5m&useYfid=true&range=' . $time;
                 $url[3] = json_decode(file_get_contents($url[0]), true);
                 $url[1] = count($url[3]['chart']['result'][0]['indicators']['quote'][0]['close']);   
                 $url[2] = "h:iA";
@@ -152,6 +152,48 @@ function timeInterval($stockName, $time)
     }
 
     return $url;
+
+    
+}
+
+
+function displayName($stockName = "", $dates, $prices,$prevClose)
+{
+
+
+    
+    $indexInfo = array();
+
+    #color code green=0 red=1 black=2
+    $index_current = $prices[count($prices)-1];
+    $index_prevClose = $prevClose;
+    $index_diff = number_format($index_current - $index_prevClose, 2);
+    $index_diffPercent = number_format((($index_current - $index_prevClose) / $index_prevClose) * 100, 2);
+    if ($index_diff > 0) {
+      $index_color = 0;
+    } else if ($index_diff < 0) {
+      $index_color = 1;
+    } else {
+      $index_color = 2;
+    }
+  
+    $indexInfo[0] = $index_color;
+    $indexInfo[1] = number_format($index_current, 2);
+    $indexInfo[2] = number_format($index_diff, 2);
+    $indexInfo[3] = number_format($index_diffPercent, 2);
+  
+    if ($indexInfo[0] == 0) {
+      echo $stockName . " " . $indexInfo[1] . "<span style='color:green'> +$indexInfo[2] (+$indexInfo[3]%)</span>" . str_repeat('&nbsp', 7);
+    } elseif ($indexInfo[0] == 1) {
+      echo $stockName . " " . $indexInfo[1] . "<span style='color:red'> $indexInfo[2] ($indexInfo[3]%)</span>" . str_repeat('&nbsp', 7);
+    } elseif ($indexInfo[0] == 2) {
+      echo $stockName . " " . $indexInfo[1] . "<span style='color:black'> $indexInfo[2] ($indexInfo[3]%)</span>" . str_repeat('&nbsp', 7);
+    }
+
+
+
+
+
 }
 
 
