b2e238bee223da37548fad95bf1e0387ad24298f
Joe <kuttehj@etown.edu>
2022-10-11 10:37:35 -0400

    edits to about

--- src/about.php
+++ src/about.php
@@ -5,7 +5,7 @@ display_small_page_heading("About", "");
 ?>
 
 <html>
-<h5> This is a semester long project for Software Engineering (EGR341) at Elizabethtown College. Over the next 12 weeks
+<h5> This is a semester long project for Software Engineering (CS341) at Elizabethtown College. Over the next 12 weeks
     Blake Hildebrand, Joe Kutteh, and Nathan Griffin take on the stock market in this exciting forcasting project!
     Blake will be doing the Beginner Page.
     Nathan will be doing the Intermediate Page.
