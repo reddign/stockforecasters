ff20d8b59f25894903d7441ccb7090dc505a628c
Joe <kuttehj@etown.edu>
2022-10-18 16:44:31 -0400

    intermediate page update, multiple stocks

--- src/intermediate.php
+++ src/intermediate.php
@@ -2,7 +2,7 @@
 require("functions/basic_html_functions.php");
 require("includes/header.php");
 require("functions/graphingFunctions.php");
-display_small_page_heading("Intermediate", "");
+display_small_page_heading("Advanced", "");
 date_default_timezone_set('America/New_York');
 ?>
 
@@ -11,7 +11,8 @@ date_default_timezone_set('America/New_York');
 <body>
     <form method="get">
         <label for="Intstock">Stock Symbol: </label>
-        <input style="width:150px; border-width:3px border-style=solid; border-color:black;" type="text" id="Intstock" name="Intstock">
+        <input style="width:100px; border-width:3px border-style=solid; border-color:black;" type="text" id="Intstock" name="Intstock">
+        <input style="width:100px; border-width:3px border-style=solid; border-color:black;" type="text" id="Intstock1" name="Intstock1">
         <label for="timeframe">Time frame:</label>
         <select name="timeframe" id="timeframe">
             <option value="none" selected disabled hidden>n/a</option>
@@ -29,11 +30,18 @@ date_default_timezone_set('America/New_York');
         </select>
         <input type="submit" name="Search" id="Search" value="Search" /> <br><br>
     </form>
+
+    <div style="font-style: italic; font-size: 13px"> 
+        - You can display the graph of up to two stocks. Leave the second text box exmpty if you only want to display one stock <br>
+        - If you want to graph a stock and indicator enter the indicator code in the second text box. <br>
+        - Indicator codes: SMA, (more codes coming soon...)
+    </div>
 </body>
 
 <?PHP
 
 if (isset($_GET['Search'])) {
+    
     $stockName = strtoupper($_GET['Intstock']);
     //this returns the array "url" which has a bunch of information that is used in later function calls
     $url = timeInterval($_GET['Intstock'], $_GET['timeframe']);
@@ -46,11 +54,46 @@ if (isset($_GET['Search'])) {
         $dates[$i] = date($url[2], $url[3]['chart']['result'][0]['timestamp'][$i]); #0=930 1=1030 2=1130 3=1230 4=130 5=230 6=330 7=400 M/D/Y H:M:S FORMAT
         $prices[$i] =  $url[3]['chart']['result'][0]['indicators']['quote'][0]['close'][$i]; #0=930 1=1030 2=1130 3=1230 4=130 5=230 6=330 7=400
     }
+    
+    if($url[2] == "h:iA"){
+        $prevClose = $url[3]['chart']['result'][0]['meta']['chartPreviousClose'];
 
-    //$prevClose = $url[3]['chart']['result'][0]['meta']['chartPreviousClose'];
-    $prevClose = $url[3]['chart']['result'][0]['indicators']['quote'][0]['close'][0];
+    } else {
+        $prevClose = $url[3]['chart']['result'][0]['indicators']['quote'][0]['close'][0];
+    }
 
-    displayGraph($stockName, $dates, $prices, $prevClose);
+
+    if(empty($_GET['Intstock1'])) {
+        //this means there is only one stock
+        displayGraph($stockName, $dates, $prices, $prevClose);
+        
+    } else {
+        //need to get data for second stock
+        $stockName1 = strtoupper($_GET['Intstock1']);
+        //this returns the array "url" which has a bunch of information that is used in later function calls
+        $url1 = timeInterval($_GET['Intstock1'], $_GET['timeframe']);
+        $url1[3] = json_decode(file_get_contents($url1[0]), true);
+    
+        $dates1 = array();
+        $prices1 = array();
+    
+        for ($i = 0; $i < $url1[1]; $i++) {
+            $dates1[$i] = date($url1[2], $url1[3]['chart']['result'][0]['timestamp'][$i]); #0=930 1=1030 2=1130 3=1230 4=130 5=230 6=330 7=400 M/D/Y H:M:S FORMAT
+            $prices1[$i] =  $url1[3]['chart']['result'][0]['indicators']['quote'][0]['close'][$i]; #0=930 1=1030 2=1130 3=1230 4=130 5=230 6=330 7=400
+        }
+        
+        if($url1[2] == "h:iA"){
+            $prevClose1 = $url1[3]['chart']['result'][0]['meta']['chartPreviousClose'];
+    
+        } else {
+            $prevClose1 = $url1[3]['chart']['result'][0]['indicators']['quote'][0]['close'][0];
+        }
+        //displayGraph($stockName, $dates, $prices, $prevClose);
+        //displayGraph($stockName1, $dates1, $prices1, $prevClose1);
+        displayGraphmultiple($stockName, $dates, $prices, $prevClose,$stockName1, $dates1, $prices1, $prevClose1);
+    }
+
+
 
 ?>
 
