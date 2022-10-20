a726a15632709f5399f23e76cc7dd412532893ed
Blake Hildebrand <hildebrandb1@etown.edu>
2022-10-20 00:16:07 -0400

    Answered FAQ

--- src/beginner.php
+++ src/beginner.php
@@ -33,7 +33,7 @@ display_small_page_heading("Beginner","");
 
 <details>
   <summary>What is a stock?</summary>
-  <p style="font-size:13px; font-style: italic;">- Answer</p>
+  <p style="font-size:13px; font-style: italic;">- A stock is a type of investment that represents a share of ownership in a company. Investors buy stocks in the hopes that they company stock price will rise, making a profit in the long term. </p>
 </details>
 
 <details>
@@ -43,17 +43,17 @@ display_small_page_heading("Beginner","");
 
 <details>
   <summary>What is an ETF?</summary>
-  <p style="font-size:13px; font-style: italic;">- Answer</p>
+  <p style="font-size:13px; font-style: italic;">- ETF, or Exchange-Traded Fund, An exchange-traded fund (ETF) is an investment instrument that comprises a basket of securities, such as stocks, bonds, derivative products, and more recently, Bitcoin futures, which can be purchased or sold in a single trade on a stock exchange.</p>
 </details>
 
 <details>
   <summary>What is the Dow Jones?</summary>
-  <p style="font-size:13px; font-style: italic;">- Answer</p>
+  <p style="font-size:13px; font-style: italic;">- Dow Jones is stock index and major source of financial news. Dow Jones groups 30 of the most traded stocks on the New York Stock Exchange. It is an index that helps investors determine the overall direction of stock prices. </p>
 </details>
 
 <details>
   <summary>How do I buy stocks?</summary>
-  <p style="font-size:13px; font-style: italic;">- Answer</p>
+  <p style="font-size:13px; font-style: italic;">- The easiest way to purchase stocks is through an online stockbroker. Open and fund an account, and then start buying!</p>
 </details>
 
 <details>
@@ -63,17 +63,17 @@ display_small_page_heading("Beginner","");
 
 <details>
   <summary>How much money do I need to but stocks?</summary> 
-  <p style="font-size:13px; font-style: italic;">- Answer</p>
+  <p style="font-size:13px; font-style: italic;">- Very little is needed to start buying stocks! An investment of 50$ can get you started, but to see any substantial profit more money is needed.</p>
 </details>  
 
 <details>
   <summary>How long should I own a particular stock for?</summary> 
-  <p style="font-size:13px; font-style: italic;">- Answer</p>
+  <p style="font-size:13px; font-style: italic;">- Every stock is different, and the stock market is ever-changing. Any particular stock can go up 50$ one day, and then drop 100$ the next day. There is no set time to keep a stock, but it is recommended to sell it when a suitable profit has been achieved.</p>
 </details>
 
 <details>
   <summary>What is an 401K and IRA?</summary>
-  <p style="font-size:13px; font-style: italic;">- Answer</p>
+  <p style="font-size:13px; font-style: italic;">- A 401K is a retirement saving plan that has tax advantages. A percentage of each paycheck is invested directly into the account, and the employer may match part or all of the contribution. IRA's are also retirement plans, but not limited to employed people. There are many different kinds of IRA, like the popular Roth IRA. </p>
 </details>
   
 
