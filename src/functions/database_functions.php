04d451ac80ae1f024eac65663ea9953518025703
Joe <kuttehj@etown.edu>
2022-10-25 11:09:17 -0400

    added databases functions

---
+++ src/functions/database_functions.php
@@ -0,0 +1,9 @@
+<?php
+function connect_to_db(){
+
+    global $database,$databasehost,$databaseuser,$databasepassword;
+    $dsn = "mysql:host=$databasehost;dbname=$database;charset=UTF8";
+    
+    $pdo = new PDO($dsn, $databaseuser, $databasepassword);
+    return $pdo;
+}
