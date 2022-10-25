567c2b8ff9f8140422da74319c15ac18e817c46b
Joe <kuttehj@etown.edu>
2022-10-25 11:15:46 -0400

    added login info

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$databasehost = "localhost";
-$database = "emcsdb";
-$databaseuser = "root";
-$databasepassword = "";
+$databasehost = "156.67.74.51";
+$database = "u413142534_stocks";
+$databaseuser = "u413142534_forecaster";
+$databasepassword = "BullzNB3ars";
 ?>
