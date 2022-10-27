38c911324ac9df7d03ceb0c44b96d62158e979ec
Joe <kuttehj@etown.edu>
2022-10-27 09:53:00 -0400

    testing stuff

--- src/testingAPI.php
+++ src/testingAPI.php
@@ -1,7 +1,21 @@
-<?PHP
-require("functions/basic_html_functions.php");
-echo "hello";
-$html = file_get_html('https://finance.yahoo.com');
-$status = $html->find('svg[class="Mend(5px) Cur(a)! Fill($tertiaryColor) Cur(p)"]', 0);
-echo $status->plaintext;
-?>
+
+<form method="get">
+    <input type="submit" name="Search1" id="Search1" value="1" />
+    <input type="submit" name="Search2" id="Search2" value="2" />
+    <input type="submit" name="Search3" id="Search3" value="3" />
+<form>
+
+        <?PHP
+
+        if (isset($_GET['Search1'])) {
+            echo "ONE";
+        }
+        if (isset($_GET['Search2'])) {
+            echo "TWO";
+        }
+        if (isset($_GET['Search3'])) {
+            echo "THREE";
+        }
+
+
+        ?>
