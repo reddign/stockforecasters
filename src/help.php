33616b64493b1a20a2f80e5d09694d7bbeafb4ed
Joe <kuttehj@etown.edu>
2022-10-26 22:06:09 -0400

    updated help, checked things off to do list

--- src/help.php
+++ src/help.php
@@ -9,6 +9,29 @@ display_small_page_heading("Help", "");
     On the left side of the website is a navagation bar.
     Each page presents their own features specific to the user.
     Users have the ability to create an account. This will allow them to save stocks to a watchlist for easy access.
+    Look below for a list of features you can find on each page! <br><br>
+
+    Beginner page <br>
+    &emsp; - Stock Ticker Search Tool <br>
+    &emsp; - FAQ Secion <br>
+    &emsp; - Stock graph explanation <br>
+    &emsp; - Popular stocks to look at <br>
+    <br>
+    Intermediate page <br>
+    &emsp; - Ability to search graph up to 2 stocks <br>
+    &emsp; - Ability to graph 1 stock and 1 indicator <br>
+    &emsp; - Stocks can be graphed on many different time frames  <br>
+    &emsp; - Popular stocks to look at <br>
+    <br>
+    Advanced page <br>
+    &emsp; - All the features from the Intermediate page <br>
+    &emsp; - News for the 1 or 2 stocks the user looks up is displayed <br>
+    &emsp; - Ability to graph stock predictions. One stock and one preiction line can be graphed at once <br>
+    &emsp; - Ability to select from multiple predictive alorgithms <br>
+    &emsp; - All stock information like 52 week range, PE, market cap, div, etc. is displayed <br>
+
+
+
 
 </h5>
 
