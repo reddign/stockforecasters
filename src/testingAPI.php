187197c1c62660c75b37d6b0d8ffbc6bb662f24d
Joe <kuttehj@etown.edu>
2022-11-03 09:13:08 -0400

    test

--- src/testingAPI.php
+++ src/testingAPI.php
@@ -1,3 +1,5 @@
+<script type="text/javascript"></script>
+
 <?php
 
 $url = 'https://query1.finance.yahoo.com/v8/finance/chart/aapl?period1=345479400&period2=1666970299&interval=1d';
@@ -15,90 +17,103 @@ for ($i = 0; $i < $numcloses; $i++) {
     $allDates[$i] = date($json['chart']['result'][0]['timestamp'][$i]); //if trading day is open the last value in this array is the CURRENT date, do -2 to get previous date    
 }
 
-$SMA_50DAY = array();
-$tempAvg = 0;
-$SMA_50DAY_INDEX = 0;
+$returnArray = array();
+$returnArray = ComputeSMA($allClosePrices,50);
+echo $returnArray[0];
 
-for($e=0;$e<50;$e++){
-    $SMA_50DAY[$e]=0;
-}
-
-
-$tempTotal = 0;
-
-
-
-for ($j = 0; $j < $numcloses-49; $j++) {
-    //This will sum and average the first 50 prices 
-    for ($i = 0; $i < 50; $i++) {
-        $tempTotal = $tempTotal +  $allClosePrices[$i + $j];
-    }
-    $tempAvg = $tempTotal / 50;
-    $SMA_50DAY[$j+50] = $tempAvg ;
-    $tempTotal = 0;
-    
-}
 ?>
 
 <div>
-        <canvas id="stockChart" height="100px"></canvas>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script>
-        const labels = <?php echo json_encode($allDates); ?>;
+    <canvas id="stockChart" height="100px"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    const labels = <?php echo json_encode($allDates); ?>;
 
-        const data = {
-            labels: labels,
-            datasets: [{
-                label: "50 DAY SMA",
-                backgroundColor: 'rgb(0, 188, 212)',
-                borderColor: 'rgb(0, 188, 212)',
-                data: <?php echo json_encode($SMA_50DAY); ?>,
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: "AAPL",
+            backgroundColor: 'rgb(0, 0, 212)',
+            borderColor: 'rgb(0, 0, 212)',
+            data: <?php echo json_encode($allClosePrices); ?>,
+        },
+        {
+            label: "average",
+            backgroundColor: 'rgb(0, 0, 44)',
+            borderColor: 'rgb(0, 0, 44)',
+            data: <?php echo json_encode($returnArray); ?>,
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+        options: {
+            plugins: {
+                legend: {
+                    display: true
+                }
             },
-            {
-                label: "AAPL",
-                backgroundColor: 'rgb(0, 0, 212)',
-                borderColor: 'rgb(0, 0, 212)',
-                data: <?php echo json_encode($allClosePrices); ?>,
-            }]
-        };
+            elements: {
+                point: {
+                    radius: 0
+                }
+            },
 
-        const config = {
-            type: 'line',
-            data: data,
-            options: {
-                plugins: {
-                    legend: {
-                        display: true
-                    }
-                },
-                elements: {
-                    point: {
-                        radius: 0
-                    }
-                },
-
-                hover: {
-                    mode: 'index',
-                    intersect: false
-                },
-                scales: {
-                    x: {
-                        ticks: {
-                            display: false
-                        },
-                        grid: {
-                            display: false
-                        }
+            hover: {
+                mode: 'index',
+                intersect: false
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        display: false
+                    },
+                    grid: {
+                        display: false
                     }
                 }
-
             }
-        };
-        const myChart = new Chart(
-            document.getElementById('stockChart'),
-            config
-        );
-    </script>
 
-    </html>
+        }
+    };
+    const myChart = new Chart(
+        document.getElementById('stockChart'),
+        config
+    );
+</script>
+
+</html>
+
+<?PHP
+
+
+
+function ComputeSMA($data, $window_size)
+{
+ 
+?>
+    <script>
+        data = <?PHP echo $data; ?>
+        window_size = <?PHP echo $window_size; ?>
+
+        let r_avgs = [],
+            avg_prev = 0;
+        for (let i = 0; i <= data.length - window_size; i++) {
+            let curr_avg = 0.00,
+                t = i + window_size;
+            for (let k = i; k < t && k <= data.length; k++) {
+                curr_avg += data[k]['price'] / window_size;
+            }
+            r_avgs.push({
+                set: data.slice(i, i + window_size),
+                avg: curr_avg
+            });
+            avg_prev = curr_avg;
+        }
+        return r_avgs;
+    </script>
+<?PHP
+}
+?>
