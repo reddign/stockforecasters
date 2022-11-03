0cd7c9b6c2ec79572589c35a5dd7df59b84af459
Joe <kuttehj@etown.edu>
2022-11-03 08:14:16 -0400

    fixed predictions php

--- src/predictions/prediction_functions.php
+++ src/predictions/prediction_functions.php
@@ -69,17 +69,27 @@ function displaySMA($stockName = "", $dates, $prices, $prevClose)
 
 }
 
-function ComputeSMA(data, window_size)
+function ComputeSMA($data, $window_size)
 {
-  let r_avgs = [], avg_prev = 0;
-  for (let i = 0; i <= data.length - window_size; i++){
-    let curr_avg = 0.00, t = i + window_size;
-    for (let k = i; k < t && k <= data.length; k++){
-      curr_avg += data[k]['price'] / window_size;
-    }
-    r_avgs.push({ set: data.slice(i, i + window_size), avg: curr_avg });
-    avg_prev = curr_avg;
-  }
-  return r_avgs;
+?>
+    <script>
+        data = <?PHP echo $data ?>
+        window_size = <?PHP echo $window_size ?>
+        let r_avgs = [],
+            avg_prev = 0;
+        for (let i = 0; i <= data.length - window_size; i++) {
+            let curr_avg = 0.00,
+                t = i + window_size;
+            for (let k = i; k < t && k <= data.length; k++) {
+                curr_avg += data[k]['price'] / window_size;
+            }
+            r_avgs.push({
+                set: data.slice(i, i + window_size),
+                avg: curr_avg
+            });
+            avg_prev = curr_avg;
+        }
+        return r_avgs;
+    </script>
+<?PHP
 }
-
