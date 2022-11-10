7c41c5724f3a0ec28ca27aeb3368129d1dc9861d
Joe <kuttehj@etown.edu>
2022-11-09 19:27:48 -0500

    sma

--- src/testingAPI.php
+++ src/testingAPI.php
@@ -1,9 +1,7 @@
-<script type="text/javascript"></script>
 
 <?php
-
 $url = 'https://query1.finance.yahoo.com/v8/finance/chart/aapl?period1=345479400&period2=1666970299&interval=1d';
-//$url = 'https://query1.finance.yahoo.com/v8/finance/chart/aapl?period1=1636402976&period2=1666970299&interval=1d';
+//$url = 'https://query1.finance.yahoo.com/v8/finance/chart/aapl?period1=1667443178&period2=1668033609&interval=1d';
 
 $json = json_decode(file_get_contents($url), true);
 $numcloses = count($json['chart']['result'][0]['indicators']['quote'][0]['close']);
@@ -15,35 +13,50 @@ $allDates = array();
 //All close prices and dates are indexed
 for ($i = 0; $i < $numcloses; $i++) {
     $allClosePrices[$i] = $json['chart']['result'][0]['indicators']['quote'][0]['close'][$i]; //if trading day is open the last value in this array is the CURRENT price, do -2 to get most recent closing price
-    $allDates[$i] = date($json['chart']['result'][0]['timestamp'][$i]); //if trading day is open the last value in this array is the CURRENT date, do -2 to get previous date    
+    $allDates[$i] = date( "M j Y",$json['chart']['result'][0]['timestamp'][$i]); //if trading day is open the last value in this array is the CURRENT date, do -2 to get previous date    
 }
 
 $j = 0;
 $h = 0;
-$SMA = array();
+$SMAtemp = array();
 $total = 0;
 
 for($j=0;$j<count($allClosePrices)-50;$j++){
 
     for($h=0;$h<50;$h++) {
-        $total = $total + $allClosePrices[$h];
+        $total = $total + $allClosePrices[$h+$j];
     }
     $total = $total/50;
-    echo $total. "<br>";
-    $SMA[$j] = $total;
+    $SMAtemp[$j] = $total;
     $total = 0;
+    
+
+}
+
+$k = 0;
+$tempArray = array();
+for($k=0;$k<50;$k++) {
+    $tempArray[$k] = null;
+}
+$SMA = array_merge($tempArray, $SMAtemp);
+//We want past 1 month of data (30 days)
+$days = 25;
+
+$p=0;
+$dates2graph = array();
+$sma2graph = array();
+$prices2graph = array();
+
+$count=0;
+
+for($p=0;$p<$days;$p++) {
+    $dates2graph[$p] = $allDates[$p+($numcloses-$days)];
+    $sma2graph[$p] = $SMA[$p+($numcloses-$days)];
+    $prices2graph[$p] = $allClosePrices[$p+($numcloses-$days)];
 
 }
 
 
-
-
-
-
-
-// $returnArray = array();
-// $returnArray = ComputeSMA($allClosePrices,50);
-// echo $returnArray[0];
 
 ?>
 
@@ -58,11 +71,18 @@ for($j=0;$j<count($allClosePrices)-50;$j++){
         labels: labels,
         datasets: [
         {
-            label: "average",
+            label: "sma",
             backgroundColor: 'rgb(0, 0, 44)',
             borderColor: 'rgb(0, 0, 44)',
             data: <?php echo json_encode($SMA); ?>,
-        }]
+        },
+        {
+            label: "aapl",
+            backgroundColor: 'rgb(0, 255, 0)',
+            borderColor: 'rgb(0, 255, 0)',
+            data: <?php echo json_encode($allClosePrices); ?>,
+        },
+]
     };
 
     const config = {
@@ -104,35 +124,3 @@ for($j=0;$j<count($allClosePrices)-50;$j++){
 </script>
 
 </html>
-
-<?PHP
-
-
-
-// function ComputeSMA($data, $window_size)
-// {
- 
-// ?>
-//     <script>
-//         data = <?PHP echo $data; ?>
-//         window_size = <?PHP echo $window_size; ?>
-
-//         let r_avgs = [],
-//             avg_prev = 0;
-//         for (let i = 0; i <= data.length - window_size; i++) {
-//             let curr_avg = 0.00,
-//                 t = i + window_size;
-//             for (let k = i; k < t && k <= data.length; k++) {
-//                 curr_avg += data[k]['price'] / window_size;
-//             }
-//             r_avgs.push({
-//                 set: data.slice(i, i + window_size),
-//                 avg: curr_avg
-//             });
-//             avg_prev = curr_avg;
-//         }
-//         return r_avgs;
-//     </script>
-// <?PHP
-// }
-?>
