eb9c42f9056dacf65b52a1e516867963d8e8117c
Joe <kuttehj@etown.edu>
2022-11-11 11:22:17 -0500

    removed label

--- src/advanced.php
+++ src/advanced.php
@@ -13,7 +13,6 @@ date_default_timezone_set('America/New_York');
 
 <body>
     <form method="get">
-        <label for="Intstock">Stock Symbol: </label>
         <input style="width:115px; border-width:3px border-style=solid; border-color:black;" type="text" id="Intstock" name="Intstock" placeholder="Stock Name">
         <input style="width:430px; border-width:3px border-style=solid; border-color:black;" type="text" id="Intstock1" name="Intstock1" placeholder="Optional: Second Stock Name or Prediction Line">
         <select name="timeframe" id="timeframe">
