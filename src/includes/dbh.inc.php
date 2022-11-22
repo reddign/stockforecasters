f42a5137ff314456f1adadd85b8248034e734dad
Joe <kuttehj@etown.edu>
2022-11-21 23:24:14 -0500

    now uses config.php so it is secure on real websit

--- src/includes/dbh.inc.php
+++ src/includes/dbh.inc.php
@@ -1,13 +1,26 @@
 <?php
 
-$serverName = "156.67.74.51";
-$dBUsername = "u413142534_forecaster";
-$dBPassword = "BullzNB3ars";
-$dBName = "u413142534_stocks";
+$databasehost = "156.67.74.51";
+$databaseuser = "u413142534_forecaster";
+$databasepassword = "BullzNB3ars";
+$database = "u413142534_stocks";
+
+// //Need to figure out how to hide this in config.php
+global $database, $databasehost, $databaseuser, $databasepassword;
 
 
-$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+$conn = mysqli_connect($databasehost, $databaseuser, $databasepassword, $database);
 
 if(!$conn){
     die("Connection Failed: " . mysqli_connect_error());
 }
+
+
+
+
+
+// $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+
+// if(!$conn){
+//     die("Connection Failed: " . mysqli_connect_error());
+// }
