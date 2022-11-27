69d84b816c712dcc595b7fbcd37de3182568bdb1
Joe <kuttehj@etown.edu>
2022-11-27 12:13:47 -0500

    footer stays at bottom now

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -2,7 +2,7 @@
 </div>
 
 <!-- W3.CSS Container -->
-<div class="w3-light-grey w3-container w3-padding-32" style="margin-top:75px;padding-right:58px"><p class="w3-right">Powered by <a href="https://www.etown.edu" title="W3.CSS" target="_blank" class="w3-hover-opacity">Etown College - Computer Science Department</a></p></div>
+<div class="w3-light-grey w3-container w3-padding-32" style="margin-top:75px;padding-right:58px; position: absolute;bottom: 0;width: 100%;"><p class="w3-right">Powered by <a href="https://www.etown.edu" title="W3.CSS" target="_blank" class="w3-hover-opacity">Etown College - Computer Science Department</a></p></div>
 
 <script>
 // Script to open and close sidebar
