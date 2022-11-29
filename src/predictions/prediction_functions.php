2b53dc35273d3b021de6453e42f83c8576c273bf
Nathan Griffin <griffinn@etown.edu>
2022-11-29 16:47:46 -0500

    pred testing 1.2

--- src/predictions/prediction_functions.php
+++ src/predictions/prediction_functions.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors','On');
 
-$anData = array(1,2,3,4,5,6,7,8,9,10);
+$anData = array(0,5,10,15,20,25,30,35,40,45,50,55);
 
 print_r(forecastHoltWinters($anData));
 
