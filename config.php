8ef3040508d00461806c255adf14576445e89b6c
Joe Kutteh <112097864+joekutteh@users.noreply.github.com>
2023-05-03 01:07:17 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$databasehost = "156.67.74.51";
-$database = "u413142534_stocks";
-$databaseuser = "u413142534_forecaster";
-$databasepassword = "BullzNB3ars";
+$databasehost = "";
+$database = "";
+$databaseuser = "";
+$databasepassword = "";
 ?>
 
